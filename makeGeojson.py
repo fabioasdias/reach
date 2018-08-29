d6ecc6addf84882d83d5e36841449978ced3fb71
Fabio Dias <fabio.dias@gmail.com>
2018-08-29 13:04:20 -0400

    better clustering

---
+++ makeGeojson.py
@@ -0,0 +1,57 @@
+import fiona
+from shapely.geometry import shape, mapping
+from shapely.ops import unary_union
+import json
+import sys
+
+if len(sys.argv)!=3:
+    print('.py shp list.tsv')
+    exit(-1)
+
+shp=sys.argv[1]
+tsv=sys.argv[2]
+
+
+wiggle=1e-5 #about half a street - used to close gaps 
+
+z2c={}
+with open(tsv,'r') as ftsv:
+    for line in ftsv:
+        z,c = line.strip().split('\t')
+        if (z[:2]=='22') or (c[:2]=='22')or (z[:2]=='20') or (c[:2]=='20'):
+            z2c[z]=c
+
+
+geoms={}
+with fiona.open(shp, 'r') as source:
+    for feat in source:
+        z=feat['properties']['ZCTA5CE10']
+        if (z in z2c):
+            c=z2c[z]
+        else:
+            continue
+            # c=-1
+
+        if c not in geoms:
+            geoms[c]=[]
+        geoms[c].append(shape(feat['geometry']).buffer(wiggle))
+
+for c in geoms:
+    print('merging ',c)
+    geoms[c]=(unary_union(geoms[c])).buffer(-1.0625*wiggle)
+    if (not geoms[c].is_valid):
+        geoms[c]=geoms[c].buffer(0)
+
+
+print('save')
+out={}
+out["type"]= "FeatureCollection"
+out["features"]= []
+for c in geoms:
+    out['features'].append({'type': "Feature",
+                            'geometry':mapping(geoms[c]), 
+                            'properties':{'centre':c}})
+
+print('saving')
+with open(tsv.replace('.tsv','.json'),'w') as fout:
+    json.dump(out,fout)#,indent=4, sort_keys=True)
--- prep.py
+++ prep.py
@@ -6,13 +6,17 @@ from geopy.distance import vincenty
 import matplotlib
 from numpy.linalg import norm
 from multiprocessing import Pool
+from os.path import exists
 
-cmap = matplotlib.cm.get_cmap('viridis')
+cmap = matplotlib.cm.get_cmap('tab20b')
 
 
 pop='./pop/pop2015.tsv'
 zbp='./zbp/out2015.tsv'
+naicsNames='./naics2012.csv'
 graphName='graph.gp'
+
+nDigits=3
 
 
 
@@ -24,53 +28,70 @@ def plotDistances(G,D,pos,seed):
     nx.draw(G,pos=pos, node_color=colors, node_size=10)
 
 
-def calcForce(s,z,W):
-    return(W[s]/(1E-9+D[s][z]**2))
-
-def doThings(G,centre,others,W):
-    D={}
-    # DR={}
-    # DR[centre]=0
-    D[centre]=0
-    todo=[centre,]
-    while todo:
-        z=todo.pop(0)
-        for zz in G.neighbors(z):
-            if (zz not in D):# and (zz not in fasterSeeds):
-                D[zz]=D[z]+1
-                # DR[zz]=DR[z]+vincenty((G.node[zz]['y'],G.node[zz]['x']),(G.node[z]['y'],G.node[z]['x'])).km
-                todo.append(zz)
-    return(D)#,DR)
+def calcForce(d,B):
+    return(B/(1E-9+d**2))
 
 if __name__=='__main__':
 
-    allNaics=getAllCodes(zbp,4)
+    allNaics=getAllCodes(zbp,nDigits)
+
+    nnames={}
+    with open(naicsNames,'r') as fin:
+        for line in fin:
+            vals=[x.strip().replace('"','') for x in line.strip().split(',')]
+            ncode=vals[0][:nDigits]
+            if (ncode not in nnames):
+                nnames[ncode]=vals[1]
+
 
     for naics in allNaics:
+        if (exists('./res/{0}.png'.format(naics))):
+            continue
         G=readNaicsGraph(graphName,pop,zbp,naics)
+        pos={}
         seeds=[]
         W={}
         for z in G.nodes():
             B=G.node[z]['B']
+            G.node[z]['s']=''
+            G.node[z]['f']=0
+            pos[z]=[G.node[z]['x'],G.node[z]['y']]
             if (B>0):
                 seeds.append(z)
                 W[z]=B
-                # if (len(seeds)>9):
-                #     break
-
 
         print(naics,'seeds',len(seeds))
-        fasterSeeds={k:True for k in seeds}
-        D={}
-        for i,s in enumerate(seeds):
-            D[s] = doThings(G,s,seeds,W) #,DR[s]
+        for z in seeds:
+            G.node[z]['D']=1E-9
+            G.node[z]['s']=z
+            G.node[z]['f']=calcForce(G.node[z]['D'],W[G.node[z]['s']])
 
-        
-        s2id={k:i for i,k in enumerate(seeds)}
-        with open('out_{0}.tsv'.format(naics),'w') as fout:
-            for z in sorted(G.nodes()):
-                cSW=[(s,calcForce(s,z,W)) for s in seeds if z in D[s]]
-                cW=[x[1] for x in cSW]
-                fout.write('\t'.join([z, cSW[np.argmax(cW/norm(cW))][0]])+'\n')
+        todo=seeds[:]
+
+        while todo:
+            z=todo.pop(0)
+            for n in G.neighbors(z):
+                if (G.node[z]['s']!=G.node[n]['s']): #different clusters
+                    maybeDist=G.node[z]['D']+vincenty((G.node[z]['y'],G.node[z]['x']),(G.node[n]['y'],G.node[n]['x'])).km
+                    maybeForce=calcForce(maybeDist,W[G.node[z]['s']])
+                    if (maybeForce>G.node[n]['f']):
+                        G.node[n]['s']=G.node[z]['s']
+                        G.node[n]['D']=maybeDist
+                        G.node[n]['f']=maybeForce
+                        todo.append(n)
+
+        colours=[]
+        allNodes=sorted(G.nodes())
+
+        with open('./res/list_{0}.tsv'.format(naics),'w') as fout:
+            for z in allNodes:
+                colours.append(G.node[z]['s'])
+                fout.write('\t'.join([z, G.node[z]['s']])+'\n')
+
+        plt.figure()                
+        plt.title(nnames[naics])
+        nx.draw(G,pos=pos,node_color=colours,nodelist=allNodes,node_size=4,cmap=cmap)
+        plt.savefig('./res/{0}.png'.format(naics),dpi=300)
+        plt.close()
 
             
